f3e6d2ab07281fc905f1a1afa84c361828e684b6
alexkozhura <alexkozhura@gmail.com>
2024-01-18 18:54:25 +0100

    Added SASS into config

--- src/lib/Counter.svelte
+++ src/lib/Counter.svelte
@@ -1,10 +1,20 @@
 <script>
-  let count = 0
-  const increment = () => {
-    count += 1
+  export let initialCount;
+  export let maxCount;
+
+  let count = initialCount;
+
+  function increment() {
+    if (count === maxCount) return;
+    count += 1;
   }
+
 </script>
 
-<button on:click={increment}>
-  count is {count}
-</button>
+<button on:click={increment}>Clicks {count}</button>
+
+<style>
+  button {
+    padding: 15px 20px;
+  }
+</style>
